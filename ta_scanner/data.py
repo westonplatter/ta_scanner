bcdb0ed2ca4a47682097882993ffc3de24280b42
westonplatter <westonplatter@gmail.com>
2020-07-03 01:52:20 -0600

    dynamic data from csvs

--- ta_scanner/data.py
+++ ta_scanner/data.py
@@ -5,5 +5,5 @@ import os
 
 def load_data(instrument_symbol: str):
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, './SPY.csv')
+    filename = os.path.join(dirname, f'./{instrument_symbol}.csv')
     return pd.read_csv(filename)
