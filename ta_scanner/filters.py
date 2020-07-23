d5cb8b1b264fe6beb6afdb3dd86b44fddc8f85f0
westonplatter <westonplatter@gmail.com>
2020-07-23 00:18:51 -0600

    code formatting

--- ta_scanner/filters.py
+++ ta_scanner/filters.py
@@ -29,7 +29,7 @@ class BaseFitler(metaclass=abc.ABCMeta):
         for fo_key in expected:
             if fo_key not in actual:
                 raise FilterException(f"expected this key key = {fo_key}")
-    
+
     @abc.abstractmethod
     def apply(self, df, field_name, filter_options):
         pass
