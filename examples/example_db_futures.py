3c9b42fb85a662d0b4e10d46576406b12b6c6bd9
westonplatter <westonplatter@gmail.com>
2020-07-28 00:59:37 -0600

    update examples to show futures data fetching

---
+++ examples/example_db_futures.py
@@ -0,0 +1,7 @@
+from loguru import logger
+from ta_scanner.data import load_and_cache, IbDataFetcher
+
+ib_data_fetcher = IbDataFetcher()
+
+df = load_and_cache("/MES", ib_data_fetcher, previous_days=20, use_rth=True)
+df = load_and_cache("/MNQ", ib_data_fetcher, previous_days=20, use_rth=True)
