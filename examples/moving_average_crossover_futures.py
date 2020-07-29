086a2b870c8ad567e84f4faf418dc2ffe017a0fc
westonplatter <westonplatter@gmail.com>
2020-07-29 01:20:51 -0600

    rename

---
+++ examples/moving_average_crossover_futures.py
@@ -0,0 +1,44 @@
+from loguru import logger
+from ta_scanner.data import load_and_cache, IbDataFetcher
+from ta_scanner.indicators import IndicatorSmaCrossover, IndicatorParams
+from ta_scanner.signals import Signal
+from ta_scanner.filters import FilterCumsum, FilterOptions, FilterNames
+from ta_scanner.reports import BasicReport
+
+
+# get SPY data
+ib_data_fetcher = IbDataFetcher()
+df = load_and_cache("/ZS", ib_data_fetcher, previous_days=30, use_rth=True)
+
+indicator_sma_cross = IndicatorSmaCrossover()
+
+# store signals in this field
+field_name = "moving_avg_cross"
+
+# Moving Average Crossover, 20 vs 50
+indicator_params = {
+    IndicatorParams.fast_sma: 10,
+    IndicatorParams.slow_sma: 30,
+}
+# apply indicator to generate signals
+indicator_sma_cross.apply(df, field_name, indicator_params)
+
+# initialize filter
+sfilter = FilterCumsum()
+
+filter_options = {
+    FilterOptions.win_points: 5,
+    FilterOptions.loss_points: 2,
+    FilterOptions.threshold_intervals: 20,
+}
+
+# generate results
+results = sfilter.apply(df, field_name, filter_options)
+
+# analyze results
+basic_report = BasicReport()
+pnl, count, average, median = basic_report.analyze(df, FilterNames.filter_cumsum.value)
+
+logger.info("------------------------")
+
+logger.info(f"Final PnL = {pnl}")
