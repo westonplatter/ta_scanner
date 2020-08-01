36ad15cbebaa27361bd093615dcf1737a1b9561b
westonplatter <westonplatter@gmail.com>
2020-08-01 13:33:56 -0600

    examples. add "return_tz" kwarg to data.load_and_cache

--- examples/example_db_futures.py
+++ examples/example_db_futures.py
@@ -1,6 +1,7 @@
 from loguru import logger
 from ta_scanner.data import load_and_cache, IbDataFetcher
 
+
 ib_data_fetcher = IbDataFetcher()
 
 future_symbols = ["/MES", "/MNQ", "/ZS", "/GC"]
@@ -8,3 +9,9 @@ future_symbols = ["/MES", "/MNQ", "/ZS", "/GC"]
 for symbol in future_symbols:
     df = load_and_cache(symbol, ib_data_fetcher, previous_days=20, use_rth=False, groupby_minutes=1)
     logger.info(f"{symbol} - {len(df.index)}")
+    logger.info(f"{symbol} - first data row = {df.head(1)}")
+
+for symbol in future_symbols:
+    df = load_and_cache(symbol, ib_data_fetcher, previous_days=20, use_rth=False, groupby_minutes=1, return_tz="US/Mountain")
+    logger.info(f"{symbol} - {len(df.index)}")
+    logger.info(f"{symbol} - first data row = {df.head(1)}")
