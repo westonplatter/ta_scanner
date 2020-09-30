afb13099d6da196887cdd4d3a551992cb7a0f7b3
westonplatter <westonplatter@gmail.com>
2020-09-30 16:16:14 -0600

    enable devs to load data from csv

--- ta_scanner/data/csv_file_fetcher.py
+++ ta_scanner/data/csv_file_fetcher.py
@@ -22,7 +22,27 @@ class CsvFileFetcher(DataFetcherBase):
         self._load_data_from_file()
 
     def _load_data_from_file(self):
-        self.df = pd.read_csv(self.file_path)
+        df = pd.read_csv(self.file_path)
+        self.df = self._prepare_columns(df)
+
+    def _prepare_columns(self, ddf):
+        rename_columns = {
+            "Date": "date",
+            " Time": "time",
+            " Open": "open",
+            " High": "high",
+            " Low": "low",
+            " Last": "close",
+            " Volume": "volume",
+            " Bid Volume": "bid_volume",
+            " Ask Volume": "ask_volume",
+            " Close": "cumulative_delta_bars"
+        }
+        ddf = ddf.rename(columns=rename_columns)
+        ddf['ts'] = pd.to_datetime(ddf['date'].map(str) + ddf['time'].map(str))
+        ddf.set_index("ts", drop=False, inplace=True)
+        ddf = ddf.tz_localize('US/Mountain')
+        return ddf
 
     def request_instrument(self):
         return self.df
